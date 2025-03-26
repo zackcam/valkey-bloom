9f6eb0c9ae22ff7631f8bdfca748ef909ce7be2d
zackcam <zackcam@amazon.com>
2025-03-26 00:06:42 +0000

    Updating must obey client so that feature flag isn't explicitly needed
    Signed-off-by: zackcam <zackcam@amazon.com>

--- src/wrapper/mod.rs
+++ src/wrapper/mod.rs
@@ -9,11 +9,17 @@ pub mod bloom_callback;
 pub fn must_obey_client(ctx: &Context) -> bool {
     #[cfg(not(feature = "valkey_8_0"))]
     {
-        // ValkeyModule_MustObeyClient exists since Valkey 8.1, so we use this as it is a more performant check.
         let ctx_raw = ctx.get_raw() as *mut valkey_module::ValkeyModuleCtx;
-
-        let status = unsafe { valkey_module::raw::ValkeyModule_MustObeyClient.unwrap()(ctx_raw) };
-        match status {
+        match unsafe {
+            if let Some(func) = valkey_module::raw::ValkeyModule_MustObeyClient {
+                func(ctx_raw)
+            } else {
+                // On Valkey 8.0, fallback to checking for replicated flag in the GetContextFlags API as a best effort.
+                return ctx
+                    .get_flags()
+                    .contains(valkey_module::ContextFlags::REPLICATED);
+            }
+        } {
             1 => true,
             0 => false,
             _ => panic!("We do not expect ValkeyModule_MustObeyClient to return anything other than 1 or 0."),
